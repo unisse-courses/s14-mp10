00525db45160d2fa44afd1b0eaca373fe80a7dd0
Thomasraf <thomas_cruz@dlsu.edu.ph>
2020-03-20 21:12:56 +0800

    added connection.js

---
+++ connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/account');
+
+mongoose.connection.once('open', function(){
+    console.log('Connection has been made!');
+}).on('error', function(error){
+    console.log('Connection error: ' + error);
+})
