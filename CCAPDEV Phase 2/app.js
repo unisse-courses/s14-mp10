dbf72f8f7b8f6011197ed147d872586d88398e0c
yungwilly <william_reyes@dlsu.edu.ph>
2020-03-20 15:57:19 +0800

    made express work

---
+++ CCAPDEV Phase 2/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('server started on port ' + app.get('port'));
+});
