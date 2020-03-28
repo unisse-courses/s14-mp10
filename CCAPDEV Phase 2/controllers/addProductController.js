dcc9575d809a34efeba2b0e606a70aaf20e1c72b
Thomasraf <thomas_cruz@dlsu.edu.ph>
2020-03-28 21:44:14 +0800

    Added modal

--- CCAPDEV Phase 2/controllers/addProductController.js
+++ CCAPDEV Phase 2/controllers/addProductController.js
@@ -5,7 +5,10 @@ const path = require('path');
 const multer = require('multer');
 const session = require('express-session');
 const cookikie = require('cookie-parser');
-const Product = mongoose.model('Product');
+var Account = mongoose.model('Account');
+var Product = require('../models/product');
+var Cart = require('../models/cart');
+var Order = require('../models/order');
 
 router.get('/', (req, res) => {
     res.render('addProduct', {
@@ -47,5 +50,52 @@ router.post('/addComment', (req,res,next)=>{
 
     res.redirect('/home')
 })
+
+router.get('/add/:id', function(req,res,next){
+    var productID = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart: {});
+
+    Product.findById(productID, (err, product)=>{
+        if(err)
+        {
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/home/checkout');
+    });
+});
+
+router.get('/reduce/:id', function(req,res,next){
+    var productID = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart: {});
+
+    cart.reduceByOne(productID);
+    req.session.cart = cart;
+    res.redirect('/home/checkout');
+});
+
+router.get('/removeAll', function(req,res,next){
+    var cart = new Cart(req.session.cart);
+
+    var order = new Order({
+        userID: req.session.id,
+        username: req.session.username,
+        cart: req.session.cart,
+        address: req.session.address,
+        contact: req.session.contactNumber
+    });
+
+    order.save((err, doc) => {
+        if(!err){
+            cart.removeAll();
+            req.session.cart = cart;
+            res.redirect('/home');
+            console.log(order);
+        }
+    });
+
+});
 
 module.exports = router;
