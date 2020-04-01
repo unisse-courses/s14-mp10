40a79fd6370fa849075ab02b3bf18981a0b329d2
Thomasraf <thomas_cruz@dlsu.edu.ph>
2020-04-01 20:12:52 +0800

    Picture in productView works now

--- CCAPDEV Phase 2/controllers/homeController.js
+++ CCAPDEV Phase 2/controllers/homeController.js
@@ -189,6 +189,18 @@ router.get('/:id', (req,res,next) => {
     })
 });
 
+// router.get('/', (req, res, next) => {
+//     Product.find(function(err, docs){
+//         var productChunks = [];
+//         var chunkSize = 3;
+//         for(var i=0; i<docs.length; i+= chunkSize)
+//         {
+//             productChunks.push(docs.slice(i, i+chunkSize));
+//         }
+//         res.render('home', {products: docs});
+//     });
+// });
+
 
 
 module.exports = router;
