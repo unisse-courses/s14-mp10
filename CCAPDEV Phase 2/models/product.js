b075790b61f39e62a4d06dbf2fa87bf3e080d616
yungwilly <william_reyes@dlsu.edu.ph>
2020-04-03 02:26:12 +0800

    Update product.js

--- CCAPDEV Phase 2/models/product.js
+++ CCAPDEV Phase 2/models/product.js
@@ -6,8 +6,8 @@ var productSchema = new mongoose.Schema({
     title: {type:String, required: true},    
     description: {type:String, required: true},
     price: {type:Number, required: true},
-    thumbsUp: {type:Number},
-    thumbsDown: {type:Number},
+    thumbsUp: {type:Number, default: 0},
+    thumbsDown: {type:Number, default: 0},
     comments: [{type: Object}]
     // comments: [{username: String, commentContent: String}]
 });
