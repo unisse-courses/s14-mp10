bbed6c2b92f2e3ab8651165fe118cab2de5f8996
Thomasraf <thomas_cruz@dlsu.edu.ph>
2020-04-12 21:19:52 +0800

    Added Procfile

--- connection.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb://localhost/account');
-
-// mongoose.connection.once('open', function(){
-//     console.log('Connection has been made!');
-// }).on('error', function(error){
-//     console.log('Connection error: ' + error);
-// })
